200a58b8831284fc073b8991285f9d20041c261c
Daniel Campbell <campbell.daniel.a@gmail.com>
2024-09-29 20:43:20 -0500

    Added a test file to learn how to code

---
+++ test.py
@@ -0,0 +1,41 @@
+
+
+def is_valid_row(row) :
+    values = [False, False, False, False, False, False, False, False, False]
+    for v in row:
+        if values[v-1]== True :
+            return False
+        values[v-1]=True
+        print(values)
+    return True    
+
+
+row = [5, 2, 5, 7, 8, 9, 3, 4, 6]
+is_valid = is_valid_row(row)
+print("Is valid row = ", is_valid)
+
+
+
+
+
+# people = ["marian", "ellie", "josie", "mom", "dad"]
+# for p in people :
+#     if p == "mom" :
+#         print(p, " is cool!")
+#     elif p == "marian" :
+#         print(p,"is super, super, super, super, super amazingly cool")
+#     else:
+#         print(p)
+  
+
+# # # this function returns 1 more apple than current apples.
+# # def get_an_apple(current_apples):
+# #     new_apples = current_apples + 1
+# #     return new_apples
+
+# # apples=0
+
+# # for i in range(10):
+# #     print("i=",i)
+# #     apples = get_an_apple(apples)
+# #     print("number of apples is ",apples)
